7c0a5dc3b2097a13d194f11dba7d4b11132a4081
Christian Moore <themooresofnott@gmail.com>
2017-12-20 22:30:09 +1100

    removed Idisposible

--- AutoMazeCS/Player.cs
+++ AutoMazeCS/Player.cs
@@ -6,7 +6,7 @@ using static AutoMazeCS.Player;
 
 namespace AutoMazeCS
 {
-    class Player : IDisposable
+    class Player
     {
         // The bot's previous co-ords, used as the spot to spawn child bots when the parent reaches an intersection.
         int PrevX;
@@ -121,7 +121,7 @@ namespace AutoMazeCS
         {
             icon = ',';
             mPlayers.Remove(this);
-            Dispose();
+            
             
         }
 
@@ -184,40 +184,7 @@ namespace AutoMazeCS
             
         }
 
-        #region IDisposable Support
-        private bool disposedValue = false; // To detect redundant calls
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects).
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-
-                disposedValue = true;
-            }
-        }
-
-        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
-        // ~Player() {
-        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-        //   Dispose(false);
-        // }
-
-        // This code added to correctly implement the disposable pattern.
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
-            Dispose(true);
-            // TODO: uncomment the following line if the finalizer is overridden above.
-            // GC.SuppressFinalize(this);
-        }
-        #endregion
+        
 
 
     }
